727d7264ca790a89a26f7056cfc191b35865a7b1
Roman Tiep Du <32752839+romanDQ@users.noreply.github.com>
2019-11-26 15:42:19 +0100

    Add files via upload

---
+++ build/precache-manifest.c631e56afc7705201af9e90ed9a6782b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cca620641aa358fec37c",
+    "url": "/static/css/main.150b4878.chunk.css"
+  },
+  {
+    "revision": "cca620641aa358fec37c",
+    "url": "/static/js/main.df606de8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "910644c5f843b8f14b1d",
+    "url": "/static/js/2.43365163.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "4b19c4926b170fd68e6abe3a1aeeed7f",
+    "url": "/index.html"
+  }
+];
